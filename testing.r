6a076a2176a19a9369723d06ac76b2fbde63da7a
Awais Haleem <awaishaleem20@gmail.com>
2020-08-04 12:24:59 +0500

    few lines added from Rstudio

--- testing.r
+++ testing.r
@@ -1,3 +1,8 @@
 # this is from git hub
 print("hello from github")
 
+name = "Awais"
+version = "these lines are written by awais"
+print(name)
+print(version)
+print("these lines are written in Rstudio.")
